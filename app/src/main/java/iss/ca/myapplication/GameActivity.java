33dbec148b6f77c8c22f5bbd92657c4cae1ed86f
Edmund-Koo <e0696911@u.nus.edu>
2021-07-14 15:12:08 +0800

    Added new links for user to fetch from

--- app/src/main/java/iss/ca/myapplication/GameActivity.java
+++ app/src/main/java/iss/ca/myapplication/GameActivity.java
@@ -7,18 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_game);
 
         Button btn = findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                Intent intent = new Intent(GameActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
